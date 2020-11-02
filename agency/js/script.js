f81c24afef8bff9ad7d161ac3781492367990e22
Mikhaylo <yunko@hotmail.co.uk>
2020-11-02 19:37:02 +0200

    Add agency

---
+++ agency/js/script.js
@@ -0,0 +1,8 @@
+function changeItem() {
+   document.getElementById("video").style.boxShadow = "-2px -14px 73px 0px rgba(0, 0, 0, 0.28)";
+} // функция, которая сработает при наведении.
+//она означает - выбрать элемент к Id у которого надо что-то изменить.
+// когда в скобки где написано one-two добавите Id своего элемента
+function rechangeItem() {
+   document.getElementById("video").style.boxShadow = "none";
+} // тут всё также. но наобарот. протсес происходящий про отводе курсора.
